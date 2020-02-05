8f20217808625d52ee8edaa2c3c44e240b14aea4
Francisco Silva <ischawk@gmail.com>
2020-02-04 18:57:10 -0600

    Updates

---
+++ precache-manifest.64f15235eedea04c580c8bb036f6129f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d44853f721bd8aa759e4966093b7ff4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0c2a5e12ad503a2f2da1",
+    "url": "/clicky-game/static/css/main.aaf23253.chunk.css"
+  },
+  {
+    "revision": "a7bc9365194ed699bbd7",
+    "url": "/clicky-game/static/js/2.ab3464b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.ab3464b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c2a5e12ad503a2f2da1",
+    "url": "/clicky-game/static/js/main.7e244ec0.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
